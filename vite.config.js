743d7cb6d996803b0e0391a756028987eda6e827
Luiz Felipe <contatolavapro@gmail.com>
2026-01-10 00:20:18 -0300

    add pwa lavaprofix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,37 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  server: {
-    host: true,
-    allowedHosts: [
-      'unsentient-postrachitic-marva.ngrok-free.dev'
-    ]
-  }
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.svg',
+        'robots.txt'
+      ],
+      manifest: {
+        name: 'LavaProFix OS',
+        short_name: 'LavaProFix',
+        description: 'Sistema técnico de ordens de serviço LavaProFix',
+        theme_color: '#E7622D',
+        background_color: '#1C1C1C',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
